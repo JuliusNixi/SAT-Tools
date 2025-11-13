58565469b5af479e1034e5161b30c54b51c6e76b
Giulio Nisi <78806318+JuliusNixi@users.noreply.github.com>
2025-11-13 09:14:10 +0100

    Added alphabet print.

--- unsatgen.py
+++ unsatgen.py
@@ -7,6 +7,7 @@ while True:
     r = sat.resolveCNF(s)
     if r is None:
         print(f"Generated an UNSAT instance with {k} clauses:")
+        print("Alphabet: " + alp)
         print(sat.stringifyCNF(s))
         print("Solution: " + str(r))
         break
